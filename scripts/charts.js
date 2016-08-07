22551ea8253db43f8d5b046f964b0463e9ba0295
timotius02 <timsitorus1@gmail.com>
2016-08-07 00:35:10 -0400

    added line chart

--- scripts/charts.js
+++ scripts/charts.js
@@ -1,1 +1,39 @@
 // line chart
+
+
+var ctx = $("#lineChart");
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    datasets: [
+        {
+            label: "Net Monthly Income",
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: false,
+        }
+    ]
+};
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: {
+        
+    }
+});
