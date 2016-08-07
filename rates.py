88d8f162c7462caf37cdd048c2c115d8f427a068
rosiezou <rosiezou@gmail.com>
2016-08-07 01:40:50 -0400

    modularization
    changed some input methods/types

--- rates.py
+++ rates.py
@@ -9,8 +9,8 @@ fiveyradjrate40k = 0.03456
 sevenyradjrate40k = 0.03412
 fiveyradjrate60k = 0.03474
 sevenyradjrate60k = 0.03443
-amtborrowed = 450000  # specified by user input
-upperlimit = 2500  # results from client data
+#amtborrowed = 450000  # specified by user input
+#upperlimit = 2500  # results from client data
 
 # v is a dictionary of discount rates with
 #    the keys being the rates and values being
@@ -23,55 +23,74 @@ for i in listofinterestrates:
         v.append(m)
 #print(discountrates)
 
-fixedpayments = []
-for i in v:
-    fixedpayments.append([(amtborrowed * i[1]/(1-i[0]**i[2]))/12, [i[1], i[2]]])
+def fixrate(amtborrowed):
+    fixedpayments = []
+    for i in v:
+        fixedpayments.append([(amtborrowed * i[1]/(1-i[0]**i[2]))/12, [i[1], i[2]]])
+    return fixedpayments
 
 #fixedpayments.sort()
 
 #fixedfinalretval = list(filter(lambda x: x[0] <= upperlimit, list(fixedpayments)))[-3:]
 #print(fixedfinalretval)
 
-adjpayments = []
-adjpayments.append([(amtborrowed * 0.03456 / (1 - 1 / (1 + fiveyradjrate40k) ** 30))/12, "with a 5-year Adjustable plan starting at 2.75%"])
-adjpayments.append([(amtborrowed * 0.03412 / (1 - 1 / (1 + sevenyradjrate40k) ** 30))/12, "with a 7-year Adjustable plan starting at 2.875%"])
-adjpayments.append([(amtborrowed * 0.03456 / (1 - 1 / (1 + fiveyradjrate60k) ** 30))/12, "with a 5-year Adjustable plan starting at 2.875%"])
-adjpayments.append([(amtborrowed * 0.03456 / (1 - 1 / (1 + sevenyradjrate60k) ** 30))/12, "with a 7-year Adjustable plan starting at 3.00%"])
+def adjrate(amtborrowed):
+    adjpayments = []
+    adjpayments.append([(amtborrowed * 0.03456 / (1 - 1 / (1 + fiveyradjrate40k) ** 30))/12, "with a 5-year Adjustable plan starting at 2.75%"])
+    adjpayments.append([(amtborrowed * 0.03412 / (1 - 1 / (1 + sevenyradjrate40k) ** 30))/12, "with a 7-year Adjustable plan starting at 2.875%"])
+    adjpayments.append([(amtborrowed * 0.03456 / (1 - 1 / (1 + fiveyradjrate60k) ** 30))/12, "with a 5-year Adjustable plan starting at 2.875%"])
+    adjpayments.append([(amtborrowed * 0.03456 / (1 - 1 / (1 + sevenyradjrate60k) ** 30))/12, "with a 7-year Adjustable plan starting at 3.00%"])
+    return adjpayments
 
-
-def insurancetype(purchase):
+def insurancetype(purchase, amtborrowed, upperlimit):
+    fp = fixrate(amtborrowed)
+    ap = adjrate(amtborrowed)
     # retval = 0
     if purchase == "house":
         # monthly payment + monthly property tax + monthly insurance
-        map(lambda x: x[0] + float(2127 / 12) + float(952 / 12), fixedpayments)
-        map(lambda x: x[0] + float(2127 / 12) + float(952 / 12), adjpayments)
+        map(lambda x: x[0] + float(2127 / 12) + float(952 / 12), fp)
+        map(lambda x: x[0] + float(2127 / 12) + float(952 / 12), ap)
         # retval = financingplansfixed(amt)[0] + float(2127/12) + float(952/12)
     elif purchase == "car":
         # monthly payment + monthly insurance
-        map(lambda x: x[0] + float(412 / 12) + float(2374 / 12), fixedpayments)
-        map(lambda x: x[0] + float(412 / 12) + float(2374 / 12), adjpayments)
+        map(lambda x: x[0] + float(412 / 12) + float(2374 / 12), fp)
+        map(lambda x: x[0] + float(412 / 12) + float(2374 / 12), ap)
         # retval = financingplansfixed(amt)[0] + float(412/12) + float(2374/12)
+    fp.sort()
+    ap.sort()
+    final = fp + ap
+    final.sort()
+    listfinal = list(filter(lambda x: x[0] <= upperlimit, list(final)))[-3:]
+    listfinal.reverse()
+    retval = []
+    for i in listfinal:
+        if isinstance(i[1], str):
+            retval.append("Your monthly payment would be " + str(round(i[0], 2)) + " " + i[1])
+            #print("Your monthly payment would be %.2f, %s" % (round(i[0], 2), i[1]))
+        else:
+            retval.append("Your monthly payment would be " + str(round(i[0], 2)) + " " + "with a " + str(i[1][1]) + "-year Fixed Rate of " + str(round(i[1][0], 3)) + "%")
+            #print("Your monthly payment would be %.2f, with a %d-year Fixed Rate of %.3f%%" % (round(i[0], 2), i[1][1], 100 * i[1][0]))
+    return retval
 
+#insurancetype("house")
 
-insurancetype("house")
+#fixedpayments.sort()
+#adjpayments.sort()
 
-fixedpayments.sort()
-adjpayments.sort()
-
-fixedfinalretval = list(filter(lambda x: x[0] <= upperlimit, list(fixedpayments)))[-3:]
+#fixedfinalretval = list(filter(lambda x: x[0] <= upperlimit, list(fixedpayments)))[-3:]
 #for i in fixedfinalretval:
 #    print("Your monthly payment would be %.2f, with a %d-year Fixed Rate of %.3f%%" % (round(i[0], 2), i[1][1], 100 * i[1][0]))
 
 
 #print(fixedfinalretval)
-adjpaymentsfinal = list(filter(lambda x: x[0] <= upperlimit, list(adjpayments)))
+#adjpaymentsfinal = list(filter(lambda x: x[0] <= upperlimit, list(adjpayments)))
 #print(adjpayments)
-final = fixedfinalretval + adjpayments
-final.sort()
-listfinal = list(filter(lambda x: x[0] <= upperlimit, list(final)))[-3:]
+#final = fixedfinalretval + adjpayments
+#final.sort()
+#listfinal = list(filter(lambda x: x[0] <= upperlimit, list(final)))[-3:]
 
-for i in listfinal:
-    if isinstance(i[1], str):
-        print("Your monthly payment would be %.2f, %s" % (round(i[0], 2), i[1]))
-    else:
-        print("Your monthly payment would be %.2f, with a %d-year Fixed Rate of %.3f%%" % (round(i[0], 2), i[1][1], 100 * i[1][0]))
+#for i in listfinal:
+#    if isinstance(i[1], str):
+#        print("Your monthly payment would be %.2f, %s" % (round(i[0], 2), i[1]))
+#    else:
+#        print("Your monthly payment would be %.2f, with a %d-year Fixed Rate of %.3f%%" % (round(i[0], 2), i[1][1], 100 * i[1][0]))
